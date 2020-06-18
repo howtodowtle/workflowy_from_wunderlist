763c6c3273823a77c159858ebdf0f7b01c1cd25c
Alex Dautel <a.j.dautel@gmail.com>
2020-06-19 01:38:34 +0200

    Fix a bug where lowest level tasks were missing

--- workflowy_importer.py
+++ workflowy_importer.py
@@ -2,30 +2,15 @@
 
 import json
 import sys
-from typing import Union
+from typing import Optional, Union
 
 
-def read_file() -> list:
-    filename = sys.argv[1]
+def read_file(filename: Optional[str] = None) -> list:
+    if not filename:
+        filename = sys.argv[1]
     with open(filename, "r", encoding="utf-8-sig") as f:
         wunderlist_export = json.load(f)
     return wunderlist_export
-
-
-def extract_list(lst: dict) -> dict:
-    listname = lst.get("title")
-    if not lst.get("tasks"):
-        return None
-    tasks = {}
-    for task in lst.get("tasks"):
-        taskname = task.get("title")
-        task_dict = {}
-        task_dict = additional_info(task, "subtasks", "title", task_dict)
-        task_dict = additional_info(task, "notes", "content", task_dict)
-        task_dict = additional_info(task, "comments", "text", task_dict)
-        if task_dict:
-            tasks.update({taskname: task_dict})
-    return {listname: tasks}
 
 
 def additional_info(task: dict, field: str, subfield: str, input_dict: dict) -> dict:
@@ -35,6 +20,26 @@ def additional_info(task: dict, field: str, subfield: str, input_dict: dict) -> 
             if ff.get(subfield):
                 input_dict.update({field: ff.get(subfield)})
     return input_dict
+
+
+def subtasks_notes_comments(task: dict) -> dict:
+    task_dict = {}
+    task_dict = additional_info(task, "subtasks", "title", task_dict)
+    task_dict = additional_info(task, "notes", "content", task_dict)
+    task_dict = additional_info(task, "comments", "text", task_dict)
+    return task_dict if task_dict else None
+
+
+def extract_list(lst: dict) -> dict:
+    listname = lst.get("title")
+    if not lst.get("tasks"):
+        return None
+    tasks = {}
+    for task in lst.get("tasks"):
+        taskname = task.get("title")
+        task_dict = subtasks_notes_comments(task)
+        tasks.update({taskname: task_dict})
+    return {listname: tasks}
 
 
 def print_next_level(
@@ -47,9 +52,9 @@ def print_next_level(
             print(f"{sep*level_count}{el}")
     elif isinstance(inpt, dict):
         for key, value in inpt.items():
+            print(f"{sep*level_count}{key}")
             if value:
                 next_level = value.splitlines() if isinstance(value, str) else value
-                print(f"{sep*level_count}{key}")
                 print_next_level(next_level, level_count + 1, sep)
 
 
@@ -72,7 +77,8 @@ def main() -> None:
         else:
             structured_dict.update(basic_list)
 
-    print_next_level(structured_dict, 0, " ")
+    sep = sys.argv[2] if len(sys.argv) > 2 else " "
+    print_next_level(structured_dict, 0, sep)
 
 
 if __name__ == "__main__":
